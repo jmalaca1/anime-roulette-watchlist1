5cec9afb213ef86949f0ec8657a539d922857036
Jacqueline MalacaraOrtega <jmalaca1@stu.southtexascollege.edu>
2026-03-08 22:03:16 -0500

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
-<script setup>
-import NavMenu from '@/components/NavMenu.vue'
-</script>
+
 
 <template>
   <div class="flex h-screen flex-col">
-    <NavMenu />
     <RouterView />
   </div>
 </template>
